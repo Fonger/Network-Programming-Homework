b5565c210959ce80635ae5e40e5a835495fe8764
Fonger <Fonger@users.noreply.github.com>
2015-10-26 06:59:37 +0800

    Add % symbol before each command.

--- lib/str_echo.c
+++ lib/str_echo.c
@@ -1,17 +1,27 @@
 #include	"unp.h"
+#include <stdio.h>
+
+void showSymbol(int sockfd);
 
 void
 str_echo(int sockfd)
 {
     ssize_t		n;
     char		buf[MAXLINE];
-    
+
 again:
-    while ( (n = read(sockfd, buf, MAXLINE)) > 0)
+    showSymbol(sockfd);
+    while ( (n = read(sockfd, buf, MAXLINE)) > 0) {
         Writen(sockfd, buf, n);
-    
+        showSymbol(sockfd);
+    }
     if (n < 0 && errno == EINTR)
         goto again;
     else if (n < 0)
         err_sys("str_echo: read error");
+}
+
+void showSymbol(int sockfd) {
+    char *symbol = "% ";
+    Writen(sockfd, symbol, 2);
 }
