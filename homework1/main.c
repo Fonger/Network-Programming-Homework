52392ac345b64c2fc3721c4c88b5a2d435a22e40
Fonger <Fonger@users.noreply.github.com>
2015-10-26 05:10:20 +0800

    Initial Commit

---
+++ homework1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  homework1
+//
+//  Created by 傑瑞 on 2015/10/26.
+//  Copyright © 2015年 傑瑞. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
